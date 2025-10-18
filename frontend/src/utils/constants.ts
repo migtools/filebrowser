385c96ddc32031382933a6221cc4630d4e0c1457
Michal Pryc <mpryc@redhat.com>
2025-10-18 23:57:22 +0200

    UPSTREAM: <carry> Add defaultLoginUser branding option
    New config option:
      --branding.defaultLoginUser "username"
    
    This option allows to provide default Username, which will result
    in hiding username field from the welcome page.
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -2,6 +2,7 @@ const name: string = window.FileBrowser.Name || "File Browser";
 const disableExternal: boolean = window.FileBrowser.DisableExternal;
 const disableUsedPercentage: boolean = window.FileBrowser.DisableUsedPercentage;
 const disableUserProfile: boolean = window.FileBrowser.DisableUserProfile;
+const defaultLoginUser: string = window.FileBrowser.DefaultLoginUser || "";
 const baseURL: string = window.FileBrowser.BaseURL;
 const staticURL: string = window.FileBrowser.StaticURL;
 const recaptcha: string = window.FileBrowser.ReCaptcha;
@@ -27,6 +28,7 @@ export {
   disableExternal,
   disableUsedPercentage,
   disableUserProfile,
+  defaultLoginUser,
   baseURL,
   logoURL,
   recaptcha,
