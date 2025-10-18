41598293fe4837d7437b03dfd32434594bb51229
Michal Pryc <mpryc@redhat.com>
2025-10-18 21:32:21 +0200

    UPSTREAM: <carry> Add disableUserProfile branding option
    New config option:
      --branding.disableUserProfile
    
    This option allows to disable User Profile together
    with User settings.
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -5,7 +5,7 @@
     <div id="nav">
       <div class="wrapper">
         <ul>
-          <router-link to="/settings/profile"
+          <router-link to="/settings/profile" v-if="!disableUserProfile"
             ><li :class="{ active: $route.path === '/settings/profile' }">
               {{ t("settings.profileSettings") }}
             </li></router-link
@@ -73,6 +73,7 @@ import { useLayoutStore } from "@/stores/layout";
 import HeaderBar from "@/components/header/HeaderBar.vue";
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
+import { disableUserProfile } from "@/utils/constants";
 
 const { t } = useI18n();
 
